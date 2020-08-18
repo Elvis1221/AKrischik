43ef4ff93defad68da29cd9ff9219e58043d15c8
Andrei <fujitsuelvis@gmail.com>
2020-08-18 14:44:40 +0300

    homework-1

--- homework-1/script.js
+++ homework-1/script.js
@@ -1,45 +1,143 @@
 // Task 1
 // найти расстояние между двумя точками
+//
+// const x1 = 30;
+// const y1 = 10;
+//
+// const x2 = 25;
+// const y2 = 8;
+//
+// const a1 = x1 - x2;
+// const b1 = y1 - y2;
+//
+// const distance = Math.sqrt(a1**2 + b1**2).toFixed(3);
+//
+// console.log(distance);
+//
+// // Task 2
+// // пусть у нас есть три числа, надо убедиться что они удовлетворяют теореме Пифагора
+//
+// const a2 = 4;
+// const b2 = 3;
+// const c2 = 5;
+//
+// console.log(`${a2**2} + ${b2**2} = ${c2**2}`,`${a2**2 + b2**2 === c2**2}`);
+//
+// // Task 3
+// // даны три числа, убедиться что они могут являться сторонами треугольника
+//
 
-const x1 = 30;
-const y1 = 10;
-
-const x2 = 25;
-const y2 = 8;
-
-const a1 = x1 - x2;
-const b1 = y1 - y2;
-
-const distance = Math.sqrt(a1**2 + b1**2).toFixed(3);
-
-console.log(distance);
-
-// Task 2
-// пусть у нас есть три числа, надо убедиться что они удовлетворяют теореме Пифагора
-
-const a2 = 4;
-const b2 = 3;
-const c2 = 5;
-
-console.log(`${a2**2} + ${b2**2} = ${c2**2}`,`${a2**2 + b2**2 === c2**2}`);
-
-// Task 3
-// даны три числа, убедиться что они могут являться сторонами треугольника
-
-
-const a3 =10 ;
-const b3 =8;
+const a3 =19 ;
+const b3 =13;
 const c3 =15;
-const itog = a3 + b3 > c3;
-console.log(itog);
 
-//либо так
+console.log(`${a3} +${b3} > ${c3}`,`${a3 + b3 > c3}`);
+console.log(`${b3} +${c3} > ${a3}`,`${b3 + c3 > a3}`);
+console.log(`${c3} +${a3} > ${b3}`,`${c3 + a3 > b3}`);
 
-// console.log(`${a3} + ${b3} > ${c3}`,`${a3 + b3 > c3}`);
 
-// либо так
 
-// console.log(a3 + b3 > c3 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const task = prompt( `Верно ли, что данные значения, могут быть сторонами трехугольника:
+// a3 =19;
+// b3 = 13;
+// c3 = 15;
+// если:
+// a3 + b3 > c3
+// b3 + c3 > a3
+// c3 + a3 > b3`);
+// alert('Ответ: ' + task)
+// const sideA3=`${a3} +${b3} > ${c3}`;
+// const sideB3=`${b3} +${c3} > ${a3}`;
+// const sideC3=`${c3} +${a3} > ${b3}`;
+//
+// alert(sideC3);
+// alert(sideA3);
+// alert(sideB3);
+//
+
+
+
+
+// alert(`${a3} +${b3} > ${c3}`,`${a3 + b3 > c3}`);
+// alert(`${b3} +${c3} > ${a3}`,`${b3 + c3 > a3}`);
+// alert(`${c3} +${a3} > ${b3}`,`${c3 + a3 > b3}`);
+
+
+
+
+
+
+
+// const sideA3 =a3 + b3 > c3;
+// const sideB3 =b3 + c3 > a3;
+// const sideC3 =c3 + a3 > b3;
+// alert()
+
+
+
+
+
+
+
+
+
+
+// const sideA3=`${a3} +${b3} > ${c3}`;
+// const sideB3=`${b3} +${c3} > ${a3}`;
+// const sideC3=`${c3} +${a3} > ${b3}`;
+// if ( sideA3 === 1>0) {
+//   alert('может быть стороной треугольника')
+// };
+// if (sideB3 === true) {
+//   alert ('эможет быть стороной треугольника')
+// };
+// if (sideC3 === `${a3 + b3 > c3}`) {
+//   alert( sideC3 >'может быть стороной треугольника')
+// };
+// console.log(sideC3);
+// console.log(sideB3);
+// console.log(sideA3);
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
